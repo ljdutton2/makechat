7f703e3a1647d3e66bd1a9406d51f92a85a72b8d
lauren <ljdutton2@icloud.com>
2020-12-02 16:59:23 -0600

    fix handlebars bug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index.handlebars');
+  res.render('layouts/main.handlebars');
 })
 
 server.listen('3000', () => {
